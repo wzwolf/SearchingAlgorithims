e34a5ef315f09d36d0a5666c0212e134466b0a56
wzwolfx <wzwolfxdev@gmail.com>
2023-01-07 15:18:17 +0800

    updated binary search, added it to run as main script

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
 
 ArrayToSearch = [66, 2, 74, 30, 68, 13, 58, 35, 10, 85, 80, 74, 15, 34, 82, 9, 72, 56]
-SortedArrayToSearch = ArrayToSearch.sort()
+ArrayToSearch.sort()
+target = 74
--- main.py
+++ main.py
@@ -1,5 +1,32 @@
 #!/usr/bin/env python3
 
 import data
+import math
 
-print(data.ArrayToSearch)
+#print(data.ArrayToSearch)
+
+def BinarySearch(array,target):
+    '''Searches a Sorted Array for index of item
+        contnues to divides array at midpoint and checks if the number belongs to which array
+
+        return index if item is found. only returns the first item
+        returns null if nothing is found'''
+    # check if array is array, check if target is valid
+
+
+    print(array)
+    print(target)
+    # create array pointers 
+    startIndex = 0
+    endIndex = len(array) - 1
+    # create output to return
+    output = -1
+    # while no target is found in array yet
+    while output == -1:
+        midwayPointer = math.floor((endIndex - startIndex)/2)
+        print(midwayPointer)
+        output = 1
+    return output
+
+if __name__ == "__main__":
+    BinarySearch(data.ArrayToSearch,data.target)
